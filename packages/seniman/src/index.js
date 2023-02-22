fcb6dae0317e99d23ce215cedc9160a540b86047
Garindra Prahandono <garindraprahandono@gmail.com>
2023-02-22 18:12:35 +0700

    feat: add withValue helper function + bump to 0.0.71

--- packages/seniman/src/index.js
+++ packages/seniman/src/index.js
@@ -2,7 +2,10 @@ import { _createBlock, _createComponent, useWindow } from './v2/window.js';
 import { useState, useMemo, onCleanup, useEffect, untrack, createContext, useContext, useCallback, runInNode, getActiveNode, onError } from './v2/state.js';
 import { _declareBlock, _declareClientFunction } from './declare.js';
 
-//import { For } from './control.js';
+function withValue(fn) {
+  return $c((e) => $s(fn)(e.target.value));
+}
+
 export {
   useState,
   useMemo,
@@ -14,6 +17,8 @@ export {
   useCallback,
   runInNode,
   getActiveNode,
+
+  withValue,
 
   onError,
   onCleanup,
