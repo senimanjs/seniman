6d3ddf60e14bbc0162e66e9a821164a67ada2fb7
Garindra <garindraprahandono@gmail.com>
2023-12-29 09:54:04 +0700

    upgrade express-basic example

--- examples/express-basic/src/index.js
+++ examples/express-basic/src/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import { createRoot } from 'seniman';
 import { wrapExpress } from 'seniman/express';
 
 let app = express();
-wrapExpress(app, { Body });
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+let root = createRoot(App);
+wrapExpress(app, root);
 
 let port = process.env.PORT || 3002;
 app.listen(port);
 
 console.log('Listening on port', port);
-
-function Body() {
-  return <div>
-    <div>Hello World</div>
-  </div>;
-}
